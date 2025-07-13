4ea9313b1e21355da3a9b181aae21ae4b953f31a
rokloc <roku.trlt@gmail.com>
2025-07-13 16:14:20 +0900

    DI

--- spring-practice/src/main/java/com/example/demo/HelloController.java
+++ spring-practice/src/main/java/com/example/demo/HelloController.java
@@ -1,18 +1,34 @@
 package com.example.demo;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HelloController {
-		
-	@RequestMapping("/")
-	public String topPage() {
-		return ("index");
+
+	/*
+	裏でDIコンテナがModelインターフェースを継承したクラスのインスタンスを
+	newして保管している。それを仮引数のmodelにポリモーフィズムの要領で代入してくれている
+	これをDI（依存性注入）といい、Springの代表的な特徴のひとつ
+	*/
+	@RequestMapping
+	public String index(Model model) { 
+		model.addAttribute("msg", "これはコントローラーに用意したメッセージです");
+		return "index";
 	}
-	@RequestMapping("/other")
-	public String otherPage() {
-		return ("other");
-	}
+	
+	
+	
+	
+	
+//	@RequestMapping("/")
+//	public String topPage() {
+//		return ("index");
+//	}
+//	@RequestMapping("/other")
+//	public String otherPage() {
+//		return ("other");
+//	}
 
 }
