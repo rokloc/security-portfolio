b4462672ccc8b9ea39afc9ddeb4a0db4d4af3232
rokloc <roku.trlt@gmail.com>
2025-07-13 11:21:13 +0900

    PathVariable

--- spring-practice/src/main/java/com/example/demo/SpringPracticeApplication.java
+++ spring-practice/src/main/java/com/example/demo/SpringPracticeApplication.java
@@ -2,12 +2,28 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringPracticeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringPracticeApplication.class, args);
 	}
+	
+	@RequestMapping("/")
+	public String index() {
+		return "Hello, Spring Boot 3!!!";
+	}
+	@RequestMapping("/{str}")
+	public String str(@PathVariable String str) {
+		return "Hello," + str + "!!!";
+	}
+	
+	
 
 }
+
