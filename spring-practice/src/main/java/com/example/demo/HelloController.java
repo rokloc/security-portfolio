c50754530b83d4cba443a192ad832b48e78a3a89
rokloc <roku.trlt@gmail.com>
2025-07-13 18:04:09 +0900

    th:action

--- spring-practice/src/main/java/com/example/demo/HelloController.java
+++ spring-practice/src/main/java/com/example/demo/HelloController.java
@@ -2,40 +2,25 @@ package com.example.demo;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
 
 @Controller
 public class HelloController {
 
-	/*
-	裏でDIコンテナがModelインターフェースを継承したクラスのインスタンスを
-	newして保管している。それを仮引数のmodelにポリモーフィズムの要領で代入してくれている
-	これをDI（依存性注入）といい、Springの代表的な特徴のひとつ
-	*/
-	@RequestMapping
-	public String index(Model model) { 
-		//テンプレートエンジンはModelの中身を辞書のように見ることができる
-		//ModelはMapのようにキーと値を保持
-		model.addAttribute("msg1", "Model_1");
-		model.addAttribute("msg2", "Model_2");
-		model.addAttribute("msg3", "Model_3");
-		model.addAttribute("msg4", "Model_4");
-		model.addAttribute("msg5", "Model_5");
-
+	@GetMapping("/")
+	public String TopPage(Model model) {
+		model.addAttribute("msg", "名前を入力して");
 		return "index";
 	}
 	
-	
-	
-	
-	
-//	@RequestMapping("/")
-//	public String topPage() {
-//		return ("index");
-//	}
-//	@RequestMapping("/other")
-//	public String otherPage() {
-//		return ("other");
-//	}
+	@PostMapping("/other")
+	public String OtherPage(@RequestParam String username,  Model model) {
+		model.addAttribute("hello", "こんにちは" + username + "さん");
+		return "other";
+	}
 
 }
