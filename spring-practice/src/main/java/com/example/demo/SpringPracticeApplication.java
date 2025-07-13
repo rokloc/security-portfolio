05b0f9e385007e35d72e7b4f412da0fc8c44f40f
rokloc <roku.trlt@gmail.com>
2025-07-13 15:19:53 +0900

    controller

--- spring-practice/src/main/java/com/example/demo/SpringPracticeApplication.java
+++ spring-practice/src/main/java/com/example/demo/SpringPracticeApplication.java
@@ -1,19 +1,38 @@
 package com.example.demo;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class SpringPracticeApplication {
-	//
+	
+	
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPracticeApplication.class, args);
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	//RestController
+	/*
 	List<String> list = new ArrayList<>(Arrays.asList("aaaaa", "bbbbb", "ccccc"));
 
 	public static void main(String[] args) {
@@ -32,8 +51,6 @@ public class SpringPracticeApplication {
 	public String str(@PathVariable int num) {
 		return "Hello," + list.get(num) + "!!!";
 	}
-	
-	
-
+	*/
 }
 
